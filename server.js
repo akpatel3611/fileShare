f0ad2071da965e5797f4f88832efb7e515d53d82
akpatel3611 <akpatel3611>
2024-12-01 14:50:33 +0530

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require ('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
+
+// Cors
+ const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // [ http://localhost:3000' , 'http://localhost:5000','http://localhost:3001']
+ }
+
+ app.use(cors(corsOptions));
 
 //Template Engine 
 app.set('views', path.join(__dirname, '/views'));
